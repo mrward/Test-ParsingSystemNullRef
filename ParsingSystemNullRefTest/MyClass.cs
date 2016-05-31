3ac59ed08002dd4bbb43c7e7bafc6c851b4dd65e
Matt Ward <matt.ward@xamarin.com>
2016-05-31 17:06:18 +0100

    Add code that produces a parsing system null ref.

---
+++ ParsingSystemNullRefTest/MyClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ParsingSystemNullRefTest
+{
+	public class MyClass
+	{
+		ViewModel viewModel;
+
+		public MyClass ()
+		{
+			viewModel = new ViewModel ();
+		}
+
+		public void Run ()
+		{
+			viewModel.LoadPackagesAsyncTask = (token) => {
+				return Task.Run	(() => throw new Exception ("Error"));
+			};
+		}
+	}
+}
+
---
+++ ParsingSystemNullRefTest/ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParsingSystemNullRefTest
+{
+	public class ViewModel
+	{
+		public Func<CancellationToken, Task> LoadPackagesAsyncTask;
+	}
+}
+
